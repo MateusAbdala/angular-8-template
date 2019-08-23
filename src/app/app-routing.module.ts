976fcca7894effcfc26650128c7a3a5cb033d8e8
Abdala <mateus.abdala>
2019-08-23 17:50:02 -0300

    PreLoader for LazyLoaded Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthLayoutComponent } from './_layout/auth-layout/auth-layout.component';
 import { ClubeLayoutComponent } from './_layout/clube-layout/clube-layout.component';
 
@@ -16,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
