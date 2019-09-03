d3b7bfa94c0461f5425f34868bece781d343c92d
Mateus Abdala <mateus.gui.abdala@gmail.com>
2019-09-02 23:26:14 -0300

    dist commenmt envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,11 @@ export const environment = {
   version: require( '../../package.json').version,
   apiUrl: 'REPLACE_WITH_API_URL'
 };
+
+/**
+ * *** NOTE ON CHANGING ENVIRONMENT VALUES ON THIS FILE ***
+ * Angular comes with environment files that allow us to handle environment specific
+ * configurations. They do not meet the requirements of a continuous delivery setup. 
+ * For easier deploys process, you can generate just one builded image and overwrite
+ * the common values such as `REPLACE_WITH_API_URL`.
+ */
